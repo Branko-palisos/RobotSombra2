d1b049995524990e9a341cabf87ef312ab562f1a
Branko-palisos <branko.palaciosm@gmail.com>
2025-10-28 21:01:17 -0400

    Started change font
    printing font index

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -4,24 +4,35 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
- public class SettingsMenu : MonoBehaviour
+public class SettingsMenu : MonoBehaviour
 {
-  // Start is called before the first frame update
-   // public Resolution[] resolutions;
-   [SerializeField]
-   Resolution[] resolutions;
-   public TMP_Dropdown resolutionDropdown;
-   public AudioMixer audioMixer;
-   public Slider volumeSlider;
-   public TextMeshProUGUI volumeQuantity;
+    // Start is called before the first frame update
+    // public Resolution[] resolutions;
+    [SerializeField]
+    Resolution[] resolutions;
+    public TMP_Dropdown resolutionDropdown;
+    public AudioMixer audioMixer;
+    public Slider volumeSlider;
+    public TextMeshProUGUI volumeQuantity;
+    [SerializeField]
+    TMP_FontAsset font;
+
+    [SerializeField]
+     TextMeshProUGUI volumeTMP;
    void Start()
-   {
+   { //var foundCanvasObjects = FindObjectsByType<CanvasRenderer>(FindObjectsSortMode.None);
+
+
+        Fonts(1031);
+        Screen.fullScreen = false;
        SetVolume();
        resolutions = Screen.resolutions;
        resolutionDropdown.ClearOptions();
-       List<string> options = new List<string>();
+       
        int currentResolutionIndex = 0;
-       for (int i = 0; i < resolutions.Length; i++)
+        List<string> options = new List<string>();
+       
+        for (int i = 0; i < resolutions.Length; i++)
        {
            string option = resolutions[i].width + "x" + resolutions[i].height;
            options.Add(option);
@@ -36,10 +47,22 @@ using UnityEngine.UI;
        resolutionDropdown.value = currentResolutionIndex;
        resolutionDropdown.RefreshShownValue();
    }
-   
+    public void Fonts(int fontIndex)
+    {
+        TextMeshProUGUI[] texts = FindObjectsByType<TextMeshProUGUI>(FindObjectsSortMode.None);
+        foreach (TextMeshProUGUI text in texts)
+        {
+            // Haz algo con cada objeto, por ejemplo, activar un componente
+           // Debug.Log("" + text);
+            Debug.Log(fontIndex);
+            text.font = font;
+        }
+      
+    }
   public void SetFullScreen(bool isFullScreen)
     {
         Screen.fullScreen = isFullScreen;       
+        
     }
    // Update is called once per frame
 
